53bce0aa5532bd3e32a092a281fde824b77a5915
deerstalker <simonchen.nju@gmail.com>
2013-07-17 16:55:25 +0800

    fix bug: format confict in windows and linux

--- database/database.bat
+++ database/database.bat
@@ -1,16 +1,16 @@
-echo ŽŽœšÊýŸÝ¿â
+echo 创建数据库
 mysql -u root -pnjueducn <"createdatabase.sql"
-echo ŽŽœšÊýŸÝ¿â±í
+echo 创建数据库表
 mysql HYDROLOGY -uroot -pnjueducn<"createtable.sql"
-echo Ïòcity±íÖÐ²åÈë³ÇÊÐÊýŸÝ
+echo 向city表中插入城市数据
 mysql HYDROLOGY -uroot -pnjueducn<"insertcity.sql"
-echo Ïòrainfall±íÖÐ²åÈëÓêÁ¿ÊýŸÝ
+echo 向城市rainfall表中插入雨量数据
 mysql HYDROLOGY -uroot -pnjueducn<"insertrainfall.sql"
-echo Ïòstationinfo±íÖÐ²åÈëºÓµÀÕŸºÍË®¿âÕŸÐÅÏ¢
+echo 向stationinfo表中插入河道站和水库站信息
 mysql HYDROLOGY -uroot -pnjueducn<"insertstationinfo.sql"
-echo Ïòstationrainfall±íÖÐ²åÈëÓêÁ¿ÊýŸÝ
+echo 向stationrainfall表中插入雨量数据
 mysql HYDROLOGY -uroot -pnjueducn<"insertstationrainfall.sql"
-echo Ïòpondage±íÖÐ²åÈëË®Î»ÊýŸÝ
+echo 向pondage表中插入水位数据
 mysql HYDROLOGY -uroot -pnjueducn<"insertpondage.sql"
 echo 向cityweather表中插入数据
 mysql HYDROLOGY -uroot -pnjueducn<"insertcityweather.sql"
